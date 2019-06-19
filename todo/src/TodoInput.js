89d76e63354d1823cbe92bda66d18deafe6bb642
Dil Sombayeva <dilyasom@gmail.com>
2019-06-19 11:01:44 -0700

    implement input form to add a new todo

--- todo/src/TodoInput.js
+++ todo/src/TodoInput.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from './actions';
 
 class TodoInput extends React.Component {
- state = {};
+ state = {
+  inputValue: '',
+ };
+
+ changeInputValue = e => {
+  this.setState({ inputValue: e.target.value });
+ };
+
+ createNewTodo = e => {
+  e.preventDefault();
+  this.props.addTodo(this.state.inputValue);
+  this.setState({ inputValue: '' });
+ };
+
  render() {
-  return <div>hi</div>;
+  return (
+   <form onSubmit={this.createNewTodo}>
+    <input
+     type="text"
+     value={this.state.inputValue}
+     onChange={this.changeInputValue}
+    />
+    <button type="submit">Add Todo</button>
+   </form>
+  );
  }
 }
 
-export default TodoInput;
+// adds props
+function mapStateToProps(state) {
+ return {};
+}
+
+export default connect(
+ mapStateToProps,
+ { addTodo: addTodo }
+ //  mapDispatchToProps
+)(TodoInput);
