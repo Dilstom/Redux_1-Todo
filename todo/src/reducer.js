aafa6de2aa27f16aaa3f143b9e4998a0a3d25f6e
Dil Sombayeva <dilyasom@gmail.com>
2019-06-19 11:02:12 -0700

    add reducer

--- todo/src/reducer.js
+++ todo/src/reducer.js
@@ -1,3 +1,6 @@
+import { ADD_TODO } from './actions';
+// import { CHANGE_TITLE } from './actions';
+
 let defaultState = {
  title: 'Welcome to Redux! And more',
  todos: [
@@ -7,7 +10,20 @@ let defaultState = {
  ],
 };
 
+// action is an object = {type: , payload}
+// state = defaultState -> if state doesn't exist then state equals 'defaltState', if state does exist it is just a state.
 export default function reducer(state = defaultState, action) {
- // state = defaultState -> if state doesn't exist then state equals 'defaltState', if state does exist it is just a state.
- return state;
+ switch (action.type) {
+  case ADD_TODO:
+   let newTodos = [...state.todos, action.payload];
+   return Object.assign({}, state, { todos: newTodos });
+  //   case CHANGE_TITLE:
+  //    // old way
+  //    //  it is going to create a new object - `{}`, it's gonna transfer state over that new object - `state`, and `{title: action.title}` will overwrite whatever was on state.title before
+  //    //    return Object.assign({}, state, { title: action.title });
+  //    // new way:
+  //    return { ...state, title: action.title };
+  default:
+   return state;
+ }
 }
