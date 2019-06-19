2d4694045be4fc15a0f4e1773d6b38a0fe49d199
Dil Sombayeva <dilyasom@gmail.com>
2019-06-19 08:18:05 -0700

    add reducer

--- todo/src/reducer.js
+++ todo/src/reducer.js
@@ -1,7 +1,13 @@
-let defaultState = {};
+let defaultState = {
+ title: 'Welcome to Redux! And more',
+ todos: [
+  {
+   todo: 'Does it work?',
+  },
+ ],
+};
 
-export default function reducer(state, action) {
- return {
-  title: 'Welcome to Redux',
- };
+export default function reducer(state = defaultState, action) {
+ // state = defaultState -> if state doesn't exist then state equals 'defaltState', if state does exist it is just a state.
+ return state;
 }
