d6ab7b354f9d725c434213c4279d2d069b7be9e0
Dil Sombayeva <dilyasom@gmail.com>
2019-06-19 11:02:02 -0700

    implement action creator

--- todo/src/actions.js
+++ todo/src/actions.js
@@ -1,5 +1,14 @@
-export const CHANGE_TITLE = 'CHANGE_TITLE';
+export const ADD_TODO = 'ADD_TODO';
 
-function changeTitle(text) {
- return { type: 'CHANGE_TITLE', title: text };
+// function changeTitle(text) {
+//  return { type: 'CHANGE_TITLE', title: text };
+// }
+
+export function addTodo(text) {
+ let newTodo = {
+  todo: text,
+  complete: false,
+ };
+
+ return { type: ADD_TODO, payload: newTodo };
 }
