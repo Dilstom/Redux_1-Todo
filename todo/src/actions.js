389f0de03483d2666e03e8ac9f5929a524b607e0
Dil Sombayeva <dilyasom@gmail.com>
2019-06-22 17:48:25 -0700

    add two more action creators

--- todo/src/actions.js
+++ todo/src/actions.js
@@ -1,5 +1,7 @@
 export const ADD_TODO = 'ADD_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
+export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
+export const DELETE_COMPLETE = 'DELETE_COMPLETE';
 
 // function changeTitle(text) {
 //  return { type: 'CHANGE_TITLE', title: text };
@@ -11,4 +13,11 @@ export function addTodo(newTodo) {
 
 export function deleteTodo(id) {
  return { type: DELETE_TODO, payload: id };
+}
+
+export function toggleComplete(id) {
+ return { type: TOGGLE_COMPLETE, payload: id };
+}
+export function deleteComplete() {
+ return { type: DELETE_COMPLETE };
 }
